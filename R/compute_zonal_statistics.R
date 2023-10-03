fe73007f8fc9cd9ae6e1c750a7528b628cf69282
Raphael Saldanha <rfsaldanha@gmail.com>
2023-10-03 11:00:36 +0200

    Do not delete the database.

--- R/compute_zonal_statistics.R
+++ R/compute_zonal_statistics.R
@@ -2,14 +2,14 @@
 #'
 #' @param zonal_tasks A tibble of zonal tasks created by `create_zonal_tasks`
 #' @param g_var Variable name (string) that unique identifies each feature at the sf object.
-#' @param db_file A path and file name for the SQLite database. If already exists, it will be deleted.
+#' @param db_file A path and file name for the SQLite database.
 #' @param cores Number of cores to use.
 #'
 #' @return An SQLite database.
 #' @export
 compute_zonal_tasks <- function(zonal_tasks, g_var, db_file, cores = 1){
   # Delete database if exists
-  if(file.exists(db_file)) unlink(db_file)
+  # if(file.exists(db_file)) unlink(db_file)
 
   # Starting message
   usethis::ui_info("Starting...")
